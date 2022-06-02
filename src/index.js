c2dda2e98471ae1074d35fea0c5e10f67df92f92
Sergey Sidorov <ssa-192@mail.ru>
2022-06-02 11:58:25 +0300

    Change text

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const typeWriter = document.getElementById('typewriter-text');
-const text = 'Lorem ipsum dolor sit amet.';
+const text = 'Consectetur adipiscing elit.';
 
 typeWriter.innerHTML = text;
 typeWriter.style.setProperty('--characters', text.length);
